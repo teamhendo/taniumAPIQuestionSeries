c0fdfd294821c65bf51820452bafe947ed11d21d
Brent Henderson <10964176+teamhendo@users.noreply.github.com>
2022-03-01 19:29:17 -0500

    Adding companion script for Conversing with TanREST Part II: Taking Action.

---
+++ part2-tanrestTakingActionCompanion.ps1
@@ -0,0 +1,30 @@
+############ Companion Snippets ############
+ 
+## Create a PSCustomObject to preserve and work with data
+
+
+$exampleObject = [PSCustomObject]@{
+	actionObject = $null
+	action       = $null
+}
+
+## Create a validated action object and store it within the actionObject property of $exampleObject
+
+$exampleObject.actionObject = New-TaniumActionObject `
+	-Name "API Series - Taking Action - $($(Get-Date).ToString("yyyyMMdd"))" `
+	-Package 'Custom Tagging - Add Tags' `
+	-Parameters @{key='$1';value=$(-join ($(Get-Date).ToString("yyyyMMdd"),'-TakingActionAPISeries'))} `
+	-Filter @(`
+		@{sensor='Windows OS Type';operator='contains';value='workstation'},`
+		@{sensor='Is Virtual';operator='contains';value='yes'}) `
+	-ActionGroup 'All Windows Workstations' `
+	-Expiration 600
+
+ 
+## Submit the constructed action object via TanREST and capture the returned data in the action property
+
+$exampleObject.action = New-TaniumAction -Data $ipuObject.actionObject
+
+## Get information about the current state of the submitted action via TanREST
+
+Get-TaniumCoreAction -ID $exampleObject.action.id
